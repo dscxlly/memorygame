7867f264165f480434d881131abf1abc7ae12b02
Julia Caroline <carolinejulia96@gmail.com>
2023-05-22 23:41:53 -0300

    download changes

--- script/script_prize.js
+++ script/script_prize.js
@@ -6,3 +6,9 @@ document.getElementById("download-btn").addEventListener("click", function(){
     window.location.href = "index.html";
 });
 
+function downloadFile() {
+    var link = document.createElement('a');
+    link.href = 'img/leaf.png';
+    link.download = 'gift';
+    link.click();
+  }
