d66990a8fa10095be6534f08ebf877436fa7f4a8
Julia Caroline <carolinejulia96@gmail.com>
2023-05-22 23:16:59 -0300

    edited song

--- script/script_prize.js
+++ script/script_prize.js
@@ -1,3 +1,12 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const audio = new Audio('song/CORALINE - Måneskin   Instrumental (Karaoke No Vocals).mp3');
+    const body = document.querySelector('body');
+  
+    body.addEventListener('click', function() {
+      audio.play();
+    });  
+});
+
 document.getElementById("open-btn").addEventListener("click", function(){
     document.getElementsByClassName("popup")[0].classList.add("active");
 });
@@ -6,11 +15,3 @@ document.getElementById("download-btn").addEventListener("click", function(){
     window.location.href = "index.html";
 });
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const audio = new Audio('song/CORALINE - Måneskin   Instrumental (Karaoke No Vocals).mp3');
-    const body = document.querySelector('body');
-  
-    body.addEventListener('click', function() {
-      audio.play();
-    });  
-  });
