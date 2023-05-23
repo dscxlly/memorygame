a86e5063e4fe955ae190313e51a5ff3b9621d5d2
Julia Caroline <carolinejulia96@gmail.com>
2023-05-22 22:47:24 -0300

    first commit

---
+++ script/script_prize.js
@@ -0,0 +1,7 @@
+document.getElementById("open-btn").addEventListener("click", function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("download-btn").addEventListener("click", function(){
+    window.location.href = "index.html";
+});
