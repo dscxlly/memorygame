90a985dae5390812cf0f1a521298c19becb5222a
something-symbolic <carolinejulia96@outlook.com>
2023-08-03 20:14:05 -0300

    download files added

--- script/script_prize.js
+++ script/script_prize.js
@@ -11,8 +11,26 @@ function downloadFile() {
     link.href = 'img/leaf.png';
     link.download = 'gift';
     link.click();
-  }*/
+  }
 
   document.getElementById("closebtn").addEventListener("click", function(){
     window.location.href = "index.html";
-});
+});*/
+
+function twoFunctions() {
+    downloadFile();
+    closebtn();
+}
+
+function downloadFile() {
+    var link = document.createElement('a');
+    link.href = 'gift/BabesGift.zip';
+    link.download = 'HappyBirthday';
+    link.click();
+};
+
+function closebtn() {
+    setTimeout(function() {
+    window.location.href = "index.html";
+}, 3000);
+};
